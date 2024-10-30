e646e925c852f38bb74a19c5fcd3956c12690d39
Chris Cannam <cannam@all-day-breakfast.com>
2024-10-30 16:46:18 +0000

    Suppress some debug output

--- framework/Document.cpp
+++ framework/Document.cpp
@@ -861,6 +861,7 @@ Document::releaseModel(ModelId modelId)
 #pragma clang diagnostic ignored "-Wpotentially-evaluated-expression"
 #endif
 
+#ifdef DEBUG_DOCUMENT
     if (auto model = ModelById::get(modelId)) {
         SVCERR << "Document::releaseModel(" << modelId << "), name "
                << model->objectName() << ", type "
@@ -868,6 +869,7 @@ Document::releaseModel(ModelId modelId)
     } else {
         SVCERR << "Document::releaseModel(" << modelId << ")" << endl;
     }
+#endif
     
     if (modelId.isNone()) {
         return;
